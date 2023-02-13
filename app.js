9ea03d1798769d1feb8ba36cba6d9817e7831f13
Dennis-221 <106723887+Dennis-221@users.noreply.github.com>
2023-02-13 17:24:55 +0530

    Update app.js for connection

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 
@@ -18,7 +19,16 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 // mongoose.connect("mongodb://localhost:27017/postsDB");
-mongoose.connect(process.env.CONN_STRING);
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.CONN_STRING);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+//mongoose.connect(process.env.CONN_STRING);
 
 // let posts = [];
 
@@ -117,6 +127,11 @@ app.get("/posts/:postName", function(req, res){
 
 });
 
-app.listen(prcocess.env.PORT || 3000, function() {
-  console.log("Server started on requested port..");
-});
+
+//Connect to the database before listening
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("listening for requests");
+    })
+})
+
